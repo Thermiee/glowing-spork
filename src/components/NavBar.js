df17955d015b4eeb02788e8f2d1bbfbead836c2f
Thermiee <navyblack16@yahoo.com>
2022-08-23 23:32:06 +0100

    fix linters error

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,28 +1,28 @@
 import React, { Component } from 'react';
 import './Navbar.css';
-import logo from '../images/icon.png' 
+import logo from '../images/icon.png';
 
-export default class NavBar extends Component { 
+export default class NavBar extends Component {
   render() {
     return (
-    <div className='navbar'>
-    <div className='nav-items'>
-            <ul>
-                <li>women</li>
-                <li>men</li>
-                <li>kids</li>
-            </ul>
+      <div className="navbar">
+        <div className="nav-items">
+          <ul>
+            <li>women</li>
+            <li>men</li>
+            <li>kids</li>
+          </ul>
         </div>
         <div>
-            <img src={logo} alt="Brand logo" />
+          <img src={logo} alt="Brand logo" />
         </div>
-        <div  className='nav-items'>
-                <ul>
-                    <li>$</li>
-                <li>cart</li>
-                </ul>
-            </div>
-            </div>
-    )
+        <div className="nav-items">
+          <ul>
+            <li>$</li>
+            <li>cart</li>
+          </ul>
+        </div>
+      </div>
+    );
   }
 }
